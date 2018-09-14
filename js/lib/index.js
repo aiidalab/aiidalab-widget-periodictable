f3d8bdea738d22168a5b486457677e93a9c82024
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-09-14 18:48:02 +0200

    Adding a CSS

--- js/lib/index.js
+++ js/lib/index.js
@@ -1,4 +1,5 @@
 // Export widget models and views, and the npm package version number.
+require('./ptable.css')
 
 module.exports = require('./ptable.js');
 module.exports['version'] = require('../package.json').version;
